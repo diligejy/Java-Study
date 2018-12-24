18030951adcbc6df68cf55f6784cef6a0611cdf3
diligejy <40735154+diligejy@users.noreply.github.com>
2018-12-25 00:45:54 +0900

    Scanner + 반복문으로 구구단 완성

--- src/MultiplicationTable/Test.java
+++ src/MultiplicationTable/Test.java
@@ -3,13 +3,14 @@ package MultiplicationTable;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("구구단 중 출력할 단은? : ");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        System.out.println("number : " + number);
-
-        int result = number * 1;                
-        // 구구단 출력하는 예제 구현   
+	public static void main(String[] args) {
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("number : " + number);
+		for (int i = 1; i < 10; i++) {
+			int result = number * i;
+			System.out.println(result);
+		}
 	}
 }
