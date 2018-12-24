51c5c65148ab350b3c155f9f6444f3ff8d0a6282
diligejy <40735154+diligejy@users.noreply.github.com>
2018-12-25 00:32:56 +0900

    구구단 구현

--- src/MultiplicationTable/Test.java
+++ src/MultiplicationTable/Test.java
@@ -1,5 +1,15 @@
 package MultiplicationTable;
 
-public class Test {
+import java.util.Scanner;
 
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("number : " + number);
+
+        int result = number * 1;                
+        // 구구단 출력하는 예제 구현   
+	}
 }
