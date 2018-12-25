f3230493ed24ae40f52657c4de6b9b77a4120a4f
diligejy <40735154+diligejy@users.noreply.github.com>
2018-12-25 23:39:00 +0900

    8강까지 했음

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -6,9 +6,10 @@ public class Calendar {
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int getmaxDaysOfMonth(int month) {
+
 		return MAX_DAYS[month - 1];
 	}
-	
+
 	public void printSampleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("-------------");
@@ -18,15 +19,22 @@ public class Calendar {
 		System.out.println("22 23 24 25 26 27 28");
 
 	}
+
 	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
 
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
+		System.out.println("반복횟수를 입력하세요");
+		int repeat = scanner.nextInt();
+
+		for (int i = 0; i < repeat; i++) {
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
+		}
+
 		scanner.close();
 
 	}
